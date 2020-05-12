eabc312eb6ddadc1824173e2efe3675b93b38c27
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-05-11 23:43:31 -0300

    alterações no bg dos projetos e bio

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var alturaTela = screen.height;
+
+var cabecalho = document.getElementById("cabecalho");
+
+console.log(cabecalho)
+console.log(alturaTela)
+
+cabecalho.style.height = String.toString(alturaTela);
+console.log(cabecalho.style.height);
+
 function direcionaGit(url) {
     window.open(url, '_blank');
 };
